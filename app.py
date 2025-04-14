19bd25194a6ec74aa533d471f02b4f5e74402483
Luis Felipe <lfcvs.rj@gmail.com>
2025-04-13 23:08:51 -0300

    update

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX, dbc_css])
-app.scripts.config.serve_locally = True
 server = app.server
 
 df = pd.read_csv("dataset_saude.csv")
